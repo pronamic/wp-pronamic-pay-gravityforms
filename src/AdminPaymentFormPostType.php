6d6b040dde2add17f1c37b504bb7338f914d626c
Remco Tolsma <info@remcotolsma.nl>
2017-02-10 10:20:04 +0100

    Auto enable new payment feeds.

--- src/AdminPaymentFormPostType.php
+++ src/AdminPaymentFormPostType.php
@@ -315,5 +315,11 @@ class Pronamic_WP_Pay_Extensions_GravityForms_AdminPaymentFormPostType {
 				delete_post_meta( $post_id, '_pronamic_pay_gf_condition_enabled' );
 			}
 		}
+
+		$active = get_post_meta( $post_id, '_pronamic_pay_gf_feed_active', true );
+
+		if ( '' === $active ) {
+			update_post_meta( $post_id, '_pronamic_pay_gf_feed_active', '1' );
+		}
 	}
 }

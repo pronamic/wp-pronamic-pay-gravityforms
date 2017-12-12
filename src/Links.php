54e5d21b005673b3f78ec4328a05dabbb21cdfff
Remco Tolsma <info@remcotolsma.nl>
2017-12-12 15:55:07 +0100

    Getting ready for version 1.6.7.

--- src/Links.php
+++ src/Links.php
@@ -7,7 +7,7 @@
  * Company: Pronamic
  *
  * @author Remco Tolsma
- * @version 1.4.4
+ * @version 1.6.7
  * @since 1.0.0
  */
 class Pronamic_WP_Pay_Extensions_GravityForms_Links {
--- src/PaymentMethodsField.php
+++ src/PaymentMethodsField.php
@@ -7,7 +7,7 @@
  * Company: Pronamic
  *
  * @author Remco Tolsma
- * @version 1.6.3
+ * @version 1.6.7
  * @since 1.4.7
  */
 class Pronamic_WP_Pay_Extensions_GravityForms_PaymentMethodsField extends GF_Field_Select {

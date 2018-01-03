65e6fd7d83a31f14bbae4d401c771a6c21ded2ad
Reüel van der Steege <reuel@pronamic.nl>
2018-01-03 10:54:43 +0100

    Happy 2018.

--- src/LeadProperties.php
+++ src/LeadProperties.php
@@ -3,7 +3,7 @@
 /**
  * Title: WordPress pay extension Gravity Forms lead properties
  * Description:
- * Copyright: Copyright (c) 2005 - 2017
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @author Remco Tolsma

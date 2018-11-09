dce8c6b6f57808222d0ae0ddc4f53bf72bad8531
Reüel van der Steege <reuel@akwaabamultimedia.nl>
2018-11-09 13:47:55 +0100

    Use taxed money.

--- src/PaymentData.php
+++ src/PaymentData.php
@@ -13,6 +13,7 @@ namespace Pronamic\WordPress\Pay\Extensions\GravityForms;
 use GFCommon;
 use Pronamic\WordPress\DateTime\DateTime;
 use Pronamic\WordPress\Money\Money;
+use Pronamic\WordPress\Money\TaxedMoney;
 use Pronamic\WordPress\Pay\Core\PaymentMethods;
 use Pronamic\WordPress\Pay\Core\Util as Core_Util;
 use Pronamic\WordPress\Pay\CreditCard;
@@ -260,7 +261,7 @@ class PaymentData extends Pay_PaymentData {
 	/**
 	 * Get (prorated) amount.
 	 *
-	 * @return Money
+	 * @return TaxedMoney
 	 */
 	public function get_amount() {
 		$amount = parent::get_amount();

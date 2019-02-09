5471179b68abaa818d6e50657bc7c77014ef945d
Reüel van der Steege <reuel@pronamic.nl>
2019-02-09 03:13:10 +0100

    Update libraries, coding standards, tests and Travis config.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,11 +8,20 @@
  * @package   Pronamic\WordPress\Pay\Extensions\GravityForms
  */
 
-/**
- * Autoload.
- */
 require_once __DIR__ . '/../vendor/autoload.php';
 
 require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
 
+/**
+ * Manually load plugin.
+ */
+function _manually_load_plugin() {
+	global $pronamic_ideal;
+
+	$pronamic_ideal = pronamic_pay_plugin();
+}
+
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Bootstrap.
 require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';

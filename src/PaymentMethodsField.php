8eff2d372c4c36f18a0669bf4c0440cb4cada3b0
Remco Tolsma <info@remcotolsma.nl>
2019-08-30 09:13:26 +0200

    2.1.10

--- src/PaymentMethodsField.php
+++ src/PaymentMethodsField.php
@@ -22,7 +22,7 @@ use Pronamic\WordPress\Pay\Plugin;
  * Company: Pronamic
  *
  * @author  Remco Tolsma
- * @version 2.1.3
+ * @version 2.1.10
  * @since   1.4.7
  *
  * @property int        $pronamicPayConfigId Added by admin.js.

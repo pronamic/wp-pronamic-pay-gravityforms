6eed4f035e69f53a1e1073ee610fe1632a80e1f9
Remco Tolsma <info@remcotolsma.nl>
2020-08-12 15:34:35 +0200

    Use new `add_phase` method.

--- src/PaymentData.php
+++ src/PaymentData.php
@@ -746,10 +746,10 @@ class PaymentData extends Pay_PaymentData {
 
 			$proration_phase = SubscriptionPhase::prorate( $regular_phase, $align_date, ( '1' === $this->feed->subscription_interval_date_prorate ) );
 
-			$subscription->phases[] = $proration_phase;
+			$subscription->add_phase( $proration_phase );
 		}
 
-		$subscription->phases[] = $regular_phase;
+		$subscription->add_phase( $regular_phase );
 
 		// Total amount.
 		$subscription->set_total_amount(

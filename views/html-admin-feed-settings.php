69a6dc8a94bd416b5cd12ccf17a9dda4a45ba774
Reüel van der Steege <reuel@pronamic.nl>
2021-05-27 15:38:53 +0200

    Fix "Fatal error: Call to a member function set_values() on bool".

--- views/html-admin-feed-settings.php
+++ views/html-admin-feed-settings.php
@@ -965,7 +965,7 @@ $feed->subscriptionFrequencyField = $pay_feed->subscription_frequency_field;
 
 							$post_id = \filter_input( INPUT_GET, 'fid', FILTER_SANITIZE_STRING );
 
-							if ( empty( $post_id ) && method_exists( $this, 'get_settings_renderer' ) ) {
+							if ( empty( $post_id ) && method_exists( $this, 'get_settings_renderer' ) && false !== $this->get_settings_renderer() ) {
 								$feed = new PayFeed( $post_id );
 
 								$this->get_settings_renderer()->set_values(
